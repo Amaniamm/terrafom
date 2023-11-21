6db93e86e0be754d08a7d54afbc3c2e002333680
amani <amanireddy154@gmail.com>
2023-11-21 15:32:54 +0530

    added

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "local_file" "pet" {
+  filename = "sample.txt"
+  content  = "we love pets!"
+}
+
+resource "random_pet" "mypet" {
+  prefix    = "Miss"
+  separator = "."
+  length    = "1"
+}
